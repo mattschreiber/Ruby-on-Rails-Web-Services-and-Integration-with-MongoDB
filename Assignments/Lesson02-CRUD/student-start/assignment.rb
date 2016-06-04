c1d09d5a970fb706df422d4d37455db9f3c6e7da
mattschreiber <matt.schreiber01@gmail.com>
2016-06-04 11:55:22 -0400

    completed Lecture one practice assignment

--- Assignments/Lesson02-CRUD/student-start/assignment.rb
+++ Assignments/Lesson02-CRUD/student-start/assignment.rb
@@ -44,14 +44,17 @@ class Solution
 
   def clear_collection
     #place solution here
+    @coll.delete_many()
   end
 
   def load_collection(file_path) 
     #place solution here
+    @coll.insert_many(Solution.load_hash(file_path))
   end
 
   def insert(race_result)
     #place solution here
+    @coll.insert_one(race_result)
   end
 
   #
