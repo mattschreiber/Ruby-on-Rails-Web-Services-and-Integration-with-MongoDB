def8f8a6d66e2f9784d7d4754295cbd63a270e63
mattschreiber <matt.schreiber01@gmail.com>
2016-06-05 16:57:12 -0400

    Completed Crud #1

--- raceday/app/models/racer.rb
+++ raceday/app/models/racer.rb
@@ -7,4 +7,15 @@ class Racer
 	def self.collection
 		self.mongo_client[:racers]
 	end
+
+	def self.all (prototype={}, sort={number:1}, skip=0, limit=nil)
+
+		if limit.nil?
+			self.collection.find(prototype).skip(skip).sort(sort)
+		else
+			self.collection.find(prototype).skip(skip).sort(sort).limit(limit)
+		end
+		
+	end
+
 end
