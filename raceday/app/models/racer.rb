99eeb23fb9e85b19034245eda220a9e961a8a53b
mattschreiber <matt.schreiber01@gmail.com>
2016-06-06 08:16:13 -0400

    added initializer

--- raceday/app/models/racer.rb
+++ raceday/app/models/racer.rb
@@ -1,4 +1,18 @@
 class Racer
+	
+	include ActiveModel::Model
+
+	attr_accessor :id, :number, :first_name, :last_name, :gender, :group, :secs
+
+	def initialize (params={})
+		@id = params[:_id].nil?  ? params[:id] : params[:_id].to_s
+		@number = params[:number].to_i
+		@first_name = params[:first_name]
+		@last_name = params[:last_name]
+		@gender = params[:gender]
+		@group = params[:group]
+		@secs = params[:secs].to_i	
+	end
 
 	def self.mongo_client
 		Mongoid::Clients.default
@@ -15,7 +29,7 @@ class Racer
 		else
 			self.collection.find(prototype).skip(skip).sort(sort).limit(limit)
 		end
-		
+
 	end
 
 end
