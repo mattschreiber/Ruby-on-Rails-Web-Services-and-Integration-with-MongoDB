0797725565c412fcd46edbe4cc57f070cb201799
mattschreiber <matt.schreiber01@gmail.com>
2016-06-16 10:26:14 -0400

    added attributes to Photo class

--- places/app/models/photo.rb
+++ places/app/models/photo.rb
@@ -1,6 +1,9 @@
 class Photo
   include ActiveModel::Model
   
+  attr_accessor :id, :location
+  attr_writer :contents
+
     #class method to connect to mongo
    def self.mongo_client
     Mongoid::Clients.default
