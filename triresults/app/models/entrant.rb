2fb898e653e0bffca1c65db6e8ac56c93b19c73c
mattschreiber <matt.schreiber01@gmail.com>
2016-06-27 10:01:27 -0400

    Entrant Relation Callback

--- triresults/app/models/entrant.rb
+++ triresults/app/models/entrant.rb
@@ -9,6 +9,18 @@ class Entrant
   field :gender, type: Placing
   field :group, type: Placing
 
-  embeds_many :results, class_name: 'LegResult', order: [:"event.o".asc]
+  embeds_many :results, class_name: 'LegResult', order: [:"event.o".asc], after_add: :update_total
+
+  # after_add do |doc|
+  # 		self.secs = 0
+  # 		update_total(doc)
+  # end
+
+  def update_total(result)
+  	self.secs = 0
+  	results.each do |result|
+			self.secs = result.secs + self.secs
+		end 
+	end
 
 end
